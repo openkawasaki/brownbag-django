2aa47f4fb825b0d212a1618fc6c3385a722e8173
Hiroshi Omata <op.homata@gmail.com>
2020-05-03 17:22:41 +0900

    Update line signature header

--- bot/views.py
+++ bot/views.py
@@ -10,6 +10,7 @@ from linebot.models import (
 )
 import os
 
+#--------------------------------------------------
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
@@ -17,20 +18,44 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 #--------------------------------------------------
+import logging
+logger = logging.getLogger('bot')
+
+#--------------------------------------------------
 @csrf_exempt
 def callback(request):
-    signature = request.META['HTTP_X_LINE_SIGNATURE']
-    body = request.body.decode('utf-8')
+    '''
+    Callback 関数
+    :param request:
+    :return:
+    '''
+
     try:
+        # get X-Line-Signature header value
+        # signature = request.META['HTTP_X_LINE_SIGNATURE']
+        signature = request.headers['X-Line-Signature']
+
+        body = request.body.decode('utf-8')
+        logger.debug("Request body: " + body)
+
         handler.handle(body, signature)
+
     except InvalidSignatureError:
+        logger.error("Invalid signature. Please check your channel access token/channel secret.")
         HttpResponseForbidden()
+
     return HttpResponse('OK', status=200)
 
 #--------------------------------------------------
-# オウム返し
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
+    '''
+    テキストメッセージ
+    :param event:
+    :return:
+    '''
+    logger.debug("handle_text_message: " + event.message.text)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
