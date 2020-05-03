42c458acb1bbee90bbf7184d6648c04aca946934
Hiroshi Omata <op.homata@gmail.com>
2020-05-03 18:26:55 +0900

    Fixed Webhook error

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseForbidden, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (
@@ -8,14 +9,14 @@ from linebot.models import (
     TextMessage,
     TextSendMessage,
 )
-import os
+import os, json
 
 #--------------------------------------------------
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET)
+handler      = WebhookHandler(CHANNEL_SECRET)
 
 #--------------------------------------------------
 import logging
@@ -38,11 +39,23 @@ def callback(request):
         body = request.body.decode('utf-8')
         logger.debug("Request body: " + body)
 
+        """
+        body_json = json.loads(body)
+        events = body_json['events']
+        for event in events:
+            message     = event['message']
+            reply_token = event['replyToken']
+        """
+
         handler.handle(body, signature)
 
     except InvalidSignatureError:
         logger.error("Invalid signature. Please check your channel access token/channel secret.")
         HttpResponseForbidden()
+
+    except Exception as e:
+        logger.error("Error: {}".format(e))
+
 
     return HttpResponse('OK', status=200)
 
@@ -54,7 +67,11 @@ def handle_text_message(event):
     :param event:
     :return:
     '''
-    logger.debug("handle_text_message: " + event.message.text)
+    #logger.debug("handle_text_message: " + event.message.text)
+
+    # see also: https://qiita.com/q_masa/items/c9db3e8396fb62cc64ed
+    if event.reply_token == "00000000000000000000000000000000":
+        return
 
     line_bot_api.reply_message(
         event.reply_token,
